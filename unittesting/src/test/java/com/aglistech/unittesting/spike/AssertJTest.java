3c61861781d014032327a52286db178900b3917e
Warteg Dust <blackmanga80@gmail.com>
2022-01-18 23:54:32 +0700

    48 only

---
+++ unittesting/src/test/java/com/aglistech/unittesting/spike/AssertJTest.java
@@ -0,0 +1,21 @@
+package com.aglistech.unittesting.spike;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class AssertJTest {
+	@Test
+	public void learning() {
+		List<Integer> numbers = Arrays.asList(12, 15, 45);
+		assertThat(numbers).hasSize(3).contains(12, 45).allMatch(x -> x > 10).allMatch(x -> x < 100)
+				.noneMatch(x -> x < 0);
+
+		assertThat("").isEmpty();
+		assertThat("ABCDE").contains("BCD").contains("ABC").startsWith("ABC").endsWith("CDE");
+
+	}
+}
